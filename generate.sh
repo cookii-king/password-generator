3ae813a83414993b28196a8126ad97ab5a8715fc
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-08-31 11:12:54 +0100

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Generate a random password
+RANDOM_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()_-+=' | fold -w 16 | head -n 1)
+
+echo "Random Password: $RANDOM_PASSWORD"
