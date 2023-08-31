611d3f4009bb48cf3e22229fd9ea61aaa3bdeec0
Samuel Ngoma <83700098+cookii-king@users.noreply.github.com>
2023-08-31 12:35:24 +0100

    Update generate-password.sh

--- generate-password.sh
+++ generate-password.sh
@@ -5,20 +5,23 @@ read -p "What type of password would you like to generate?
 2. Password for JKS
 Enter your choice: " choice
 
+read -p "Enter the password length (default: 16): " password_length
+
+# Set default password length to 16 if no input provided
+password_length=${password_length:-16}
+
 case $choice in
     1)
         # Generate a random password
-        RANDOM_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9!@#$%^&*()_-+=' | fold -w 16 | head -n 1)
+        RANDOM_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9!@%^&*_-+=' | fold -w "$password_length" | head -n 1)
         echo "Random Password: $RANDOM_PASSWORD"
         ;;
     2)
-        read -p "Enter the password length for JKS: " password_length
-        # Generate a password for JKS with valid characters
-        JKS_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9' | fold -w "$password_length" | head -n 1)
+        # Generate a password for JKS with supported special characters
+        JKS_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc 'a-zA-Z0-9!@%^&*_-+=' | fold -w "$password_length" | head -n 1)
         echo "JKS Password: $JKS_PASSWORD"
         ;;
     *)
         echo "Invalid choice"
         ;;
 esac
-
